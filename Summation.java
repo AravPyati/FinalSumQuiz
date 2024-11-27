1b0ed6e8c9695b0c068b3b99b616f28d33bfb135
Zahin Chowdhury <zahinhasan@outlook.com>
2024-11-27 13:26:28 -0500

    Added lamba expressions to main calculations

--- Summation.java
+++ Summation.java
@@ -1,5 +1,16 @@
 public class Summation{
   public static void main(String[] args){
-  
+    NumAdd adder = (start, end) -> {
+      int sum = 0;
+      for(int i = start; i <= end; i++) {
+        sum += i;
+      }
+      return sum;
+    };
+
+    int start = Integer.parseInt(args[0]);
+    int end = Integer.parseInt(args[1]);
+
+    System.out.println("Sum: " + adder.addNumbers(start, end));
   }
 }
